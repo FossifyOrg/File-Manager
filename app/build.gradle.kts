a19efbda608352cd04daa666002a7b4d21e86676
Ensar Sarajčić <dev@ensarsarajcic.com>
2024-09-28 12:40:20 +0200

    Add reusable workflows for PRs and testing builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ import java.io.FileInputStream
 plugins {
     alias(libs.plugins.android)
     alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.detekt)
 }
 
 val keystorePropertiesFile: File = rootProject.file("keystore.properties")
@@ -89,8 +90,14 @@ android {
 
     lint {
         checkReleaseBuilds = false
-        abortOnError = false
+        abortOnError = true
+        warningsAsErrors = true
+        baseline = file("lint-baseline.xml")
     }
+}
+
+detekt {
+    baseline = file("detekt-baseline.xml")
 }
 
 dependencies {
